88d8bceaa9ad10a2337add2ee5b63eddad62e086
Borja Cabeza <borjacabezarozas@gmail.com>
2025-05-14 19:07:32 +0200

    Finalización del proyecto CosmosDB

--- 04-CosmosDB/Python/demo-CosmoDB/app.py
+++ 04-CosmosDB/Python/demo-CosmoDB/app.py
@@ -5,8 +5,8 @@ import uuid
 
 app = Flask(__name__)
 
-COSMOS_URL = "https://demonosqlbcr.documents.azure.com:443/"
-COSMOS_KEY = "GTUisV9jVwcGE4CLNJe3ZQRW3U15r9di8wnCLtNedD1jrQfOjl6Y22Wbyl6hSvWpg5ZhgHW1DbMyACDbTAOLCw=="
+COSMOS_URL = "<cosmos endpoint>"
+COSMOS_KEY = "<cosmos key>"
 COSMOS_DB = "NorthwindDB"
 COSMOS_CONTAINER = "Products"
 
@@ -19,6 +19,30 @@ database = client.create_database_if_not_exists(COSMOS_DB)
 container = database.create_container_if_not_exists(
     id=COSMOS_CONTAINER,
     partition_key=PartitionKey("/categoria"))
+
+@app.route("/editar/<id>/<categoria>", methods=["GET", "POST"])
+def editar(id, categoria):
+    if request.method == "GET":
+        query = f"SELECT * FROM c WHERE c.id = '{id}'"
+        items = list(container.query_items(query=query, partition_key=categoria))
+        return render_template("editar.html", producto=items[0])
+    else:
+        nombre = request.form.get("nombre")
+        categoria = request.form.get("categoria")
+        precio = request.form.get("precio")
+        stock = request.form.get("stock")
+
+        producto = {
+            "id": id,
+            "nombre": nombre,
+            "categoria": categoria,
+            "precio": float(precio),
+            "stock": int(stock)
+        }
+
+        container.replace_item(item=id, body=producto)
+
+        return redirect("/")
 
 @app.route("/registrar", methods=["GET", "POST"])
 def registrar():
@@ -42,7 +66,37 @@ def registrar():
 
     return redirect("/")
 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("listado.html", categorias=listado_categorias())
 
+@app.route("/buscar", methods=["POST"])
+def buscar():
+    nombre = request.form.get("nombre")
+    categoria = request.form.get("categoria")
+
+    if(nombre == ""):
+        query = "SELECT * FROM c"
+    else:
+        query = f"SELECT * FROM c WHERE CONTAINS(LOWER(c.nombre), LOWER('{nombre}'))"
+
+    if(categoria == "all"):    
+        items = list(container.query_items(query=query, enable_cross_partition_query=True))
+    else:
+        items = list(container.query_items(query=query, partition_key=categoria))    
+
+    return render_template("listado.html", productos=items, categorias=listado_categorias())
+
+@app.route("/eliminar/<id>/<categoria>", methods=["GET"])
+def eliminar(id, categoria):
+    container.delete_item(item=id, partition_key=categoria)
+    return redirect("/")
+
+def listado_categorias():
+    query = "SELECT DISTINCT c.categoria FROM c ORDER BY c.categoria"
+    items = list(container.query_items(query=query, enable_cross_partition_query=True))
+
+    return items
 
 
 app.run(debug=True)
